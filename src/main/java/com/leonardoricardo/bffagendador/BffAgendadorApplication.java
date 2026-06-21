f5ef159ce9899a85a834a56f48722078e1d235cb
leonardoricardodev98 <leonardoricardo2087@gmail.com>
2026-06-21 18:36:15 -0300

    adicionei o esqueleto do projeto

---
+++ src/main/java/com/leonardoricardo/bffagendador/BffAgendadorApplication.java
@@ -0,0 +1,13 @@
+package com.leonardoricardo.bffagendador;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BffAgendadorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BffAgendadorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/leonardoricardo/bffagendador/BffAgendadorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.leonardoricardo.bffagendador;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BffAgendadorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
